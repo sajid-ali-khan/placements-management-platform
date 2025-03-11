bedfff1772ed77d6e95b0829c3796549dd037681
sajid@windows <sajid@windows>
2025-03-12 00:11:24 +0530

    added necessary admin files, completed profile page

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,22 @@ const router = express.Router();
 const { authenticate, authorizeRole } = require("../middlewares/authMiddleware");
 
 router.get("/", authenticate, authorizeRole("ADMIN"), (req, res) => {
-    res.render("dashboard-admin")
+    res.render("dashboard-admin", {body: "admin-profile"})
 });
+
+router.get("/profile", authenticate, authorizeRole("ADMIN"), (req, res) => {
+    res.render("dashboard-admin", {body: "admin-profile"})
+})
+
+router.get("/companies", authenticate, authorizeRole("ADMIN"), (req, res) => {
+    res.render("dashboard-admin", {body: "managing_companies"})
+})
+
+router.get("/students", authenticate, authorizeRole("ADMIN"), (req, res) => {
+    res.render("dashboard-admin", {body: "managing_students"})
+})
+
+router.get("/placement_records", authenticate, authorizeRole("ADMIN"), (req, res) => {
+    res.render("dashboard-admin", {body: "placement_records"})
+})
 module.exports = router;
