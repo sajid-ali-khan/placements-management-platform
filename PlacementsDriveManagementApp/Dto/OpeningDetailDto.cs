07222dac074704418dbed2180e1dccb51aa4c3c2
sajid@windows <sajid@windows>
2025-03-22 21:43:36 +0530

    minor update

--- PlacementsDriveManagementApp/Dto/OpeningDetailDto.cs
+++ PlacementsDriveManagementApp/Dto/OpeningDetailDto.cs
@@ -6,7 +6,7 @@
         public string JobTitle { get; set; }
         public string Description { get; set; }
         public string CreatedDate { get; set; }
-        public String LastDate { get; set; }
+        public string LastDate { get; set; }
         public bool IsActive { get; set; }
         public string CompanyName { get; set; }
     }
