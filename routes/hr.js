53a201771f147d56f23dff0164e05d9bcf4996b3
sajid@windows <sajid@windows>
2025-03-23 12:44:00 +0530

    updated hr dashboard ui

--- routes/hr.js
+++ routes/hr.js
@@ -3,15 +3,7 @@ const router = express.Router();
 const { authenticate, authorizeRole } = require("../middlewares/authMiddleware");
 
 router.get("/", authenticate, authorizeRole("HR"), (req, res) => {
-    res.render("dashboard-hr")
-});
-
-router.get("/opening", authenticate, authorizeRole("HR"), (req, res) => {
-    res.render("dashboard-hr", {body: "hr-opening"});
-})
-
-router.get("/profile", authenticate, authorizeRole("HR"), (req, res) => {
     res.render("dashboard-hr", {body: "hr-profile"})
-})
+});
 
 module.exports = router;
